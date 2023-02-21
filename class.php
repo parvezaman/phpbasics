7cd099d4b87e481312e0695f233fd12fb6b57f86
parvezaman <emailtoamanullah@gmail.com>
2023-02-21 10:13:49 +0600

    understood inheritance and access modifiers

--- class.php
+++ class.php
@@ -4,12 +4,15 @@ class Phone
     public $model;
     public $color;
     public $manufacturer;
+    protected $batteryLife;
+    private $weight;
 
-    public function __construct($phoneColor, $phoneModel, $phoneManufacturer)
+    public function __construct($phoneColor, $phoneModel, $phoneManufacturer, $weight)
     {
         $this->color = $phoneColor;
         $this->model = $phoneModel;
         $this->manufacturer=$phoneManufacturer;
+        $this->weight = $weight;
     }
 
     public function printObject()
@@ -23,7 +26,7 @@ class Phone
     }
 }
 
-$iPhone6 = new Phone("black", "i phone 6", "Apple");
+$iPhone6 = new Phone("black", "i phone 6", "Apple", "213 gm");
 echo "<br>";
 $iPhone6->printObject();
 echo "<br>";
@@ -54,3 +57,43 @@ class Fruit
 
 $apple = new Fruit("Guava");
 echo $apple->get_name();
+
+
+// trying access modifiers
+// you can access the public anywhere
+$iPhone6 -> model = "nokia 1100";
+echo "<br>";
+echo $iPhone6 -> model;
+
+// protected method set_batteryLife() only can be accessed inside an inheritant
+class featurePhone extends Phone
+{
+    public function set_batteryLife($batteryLife)
+    {
+        $this->batteryLife = $batteryLife;
+    }
+
+    public function print_batteryLife()
+    {
+        return $this->batteryLife;
+    }
+
+    public function set_weight($weight)
+    {
+        $this->weight= $weight;
+    }
+    public function print_weight()
+    {
+        return $this->weight;
+    }
+}
+
+$nokia1250 = new featurePhone("black", "Nokia 1250", "Nokia", "213 gm");
+
+$nokia1250->set_batteryLife("4500 nmAh");
+$nokia1250->model="Nokia 1250";
+echo "<br>";
+echo $nokia1250 -> print_batteryLife(); // you cant call outside the inheritant because of its being protected proterty
+echo "<br>";
+$nokia1250->set_weight("213 gm");
+echo $nokia1250 -> print_weight(); // you cant call outside the inheritant because of its being protected proterty
